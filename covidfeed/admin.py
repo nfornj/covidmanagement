1fee27dabafd88d4227f5954a3ca3657c4d3d647
nfornj <nfornj@gmail.com>
2021-05-06 17:22:43 -0500

    model update

--- covidfeed/admin.py
+++ covidfeed/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from covidfeed.models import Plasma,PlasmaTopic
+from covidfeed.models import Plasma,Topic,Oxygen,Bed
 
 # Register your models here.
 
 admin.site.register(Plasma)
-admin.site.register(PlasmaTopic)
+admin.site.register(Topic)
+admin.site.register(Oxygen)
+admin.site.register(Bed)
+
 
 
--- covidfeed/models.py
+++ covidfeed/models.py
@@ -3,26 +3,45 @@ from django.db import models
 # Create your models here.
 
 
-class PlasmaTopic(models.Model):
+class Topic(models.Model):
 
-    plasma_topic_name = models.CharField(max_length=264)
+    topic_name = models.CharField(max_length=264)
 
     def __str__(self):
-        return self.plasma_topic_name
-
+        return self.topic_name
 
 class Plasma(models.Model):
-
-
     plasma_user_name = models.CharField(max_length=50) # social media user
-    plasma_topic_name = models.ForeignKey(PlasmaTopic,on_delete=models.CASCADE)
-    social_media_content = models.CharField(max_length=1024) # content of data
-    social_media = models.CharField(max_length=25) ##twitter, facebook etc... 
+    plasma_topic_name = models.ForeignKey(Topic,on_delete=models.CASCADE)
+    plasma_social_media_content = models.CharField(max_length=1024) # content of data
     plasma_request_date = models.DateField() #date created
+    plasma_user_location = models.CharField(max_length=50,default=' ') # social media user
 
 
     def __str__(self):
         return self.plasma_user_name
+
+class Oxygen(models.Model):
+    oxygen_user_name = models.CharField(max_length=50) # social media user
+    oxygen_topic_name = models.ForeignKey(Topic,on_delete=models.CASCADE)
+    oxygen_social_media_content = models.CharField(max_length=1024) # content of data
+    oxygen_request_date = models.DateField() #date created
+    oxygen_user_location = models.CharField(max_length=50,default=' ') # social media user
+
+    def __str__(self):
+        return self.oxygen_user_name
+
+class Bed(models.Model):
+    bed_user_name = models.CharField(max_length=50) # social media user
+    bed_topic_name = models.ForeignKey(Topic,on_delete=models.CASCADE)
+    bed_social_media_content = models.CharField(max_length=1024) # content of data
+    bed_request_date = models.DateField() #date created
+    bed_user_location = models.CharField(max_length=50,default=' ') # social media user
+
+
+    def __str__(self):
+        return self.bed_user_name
+
 
 
 
